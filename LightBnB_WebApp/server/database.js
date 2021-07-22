e86f2286f4b9c9569d626ae9cf542b5842624f96
mohammedpythons <mohammedshokai99@gmail.com>
2021-07-22 19:26:42 -0400

    adding new property to the database

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -83,7 +83,7 @@ const getAllProperties = function(options, limit = 10) {
   }
   if (options.owner_id) {
     queryparams.push(options.owner_id)
-    queryString += `AND owner_id = $${owner_id}`
+    queryString += `AND owner_id $${owner_id}`
   }
   if (options.minimum_rating) {
     queryparams.push(`${options.minimum_rating}`)
@@ -110,11 +110,6 @@ const getAllProperties = function(options, limit = 10) {
   LIMIT $${queryparams.length};
   `;
 
-  if (options.owner_id) {
-    queryparams.push(`${options.owner_id}`)
-    queryString += ``
-  }
-
   console.log(queryString, queryparams);
   console.log(options)
 
@@ -130,9 +125,10 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  let queryString = `INSERT INTO properties (title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street,  city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms, owner_id)
+  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) RETURNING *;`
+  const queryparams = [];
+  queryparams.push(property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night, property.street, property.city, property.province, property.post_code, property.country, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms, property.owner_id)
+  return pool.query(queryString, queryparams).then(res => res.rows).catch(err => {console.log(err.message)})
 }
 exports.addProperty = addProperty;
